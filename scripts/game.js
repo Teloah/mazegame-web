51c07994c4e4175dca3bb13ae687d848721c0c20
Andris Klaipiņš <teloah@gmail.com>
2015-04-19 19:32:45 +0300

    added game loop

--- scripts/game.js
+++ scripts/game.js
@@ -7,22 +7,44 @@ var canvasGame = document.getElementById("background"),
 		darkbricks: 28,
 		ground: 54
 	},
+	lastLoopTime = new Date(),
+	elapsedSecs = 0,
+	requestAnimFrame = window.requestAnimationFrame,
 	imgSprites = new Image();
 imgSprites.src = "images/sprites.png";
-imgSprites.addEventListener("load", init, false);
+imgSprites.onload = init;
 
 function init() {
 	// contextGame.globalAlpha = 0.5;
+	requestAnimFrame(gameLoop);
+	console.log("initialized");
+}
+
+function paintBorders() {
 	paintImageRow("darkbricks", 0, 720, 0);
 	paintImageColumn("darkbricks", 24, 480 - 24, 0);
 	paintImageColumn("darkbricks", 24, 480 - 24, 720 - 24);
 	paintImageRow("darkbricks", 0, 720, 480 - 24);
 	
 	fillImageRect("ground", 24, 24, 720 - 24, 480 - 24);
-	
-	console.log("initialized");
 }
 
+function gameLoop() {
+	contextGame.clearRect(0, 0, canvasGame.width, canvasGame.height);
+	paintBorders();
+	paintElapsedTime();
+	requestAnimFrame(gameLoop);
+}
+
+function paintElapsedTime() {
+	var tickTime = new Date();
+	if (tickTime - lastLoopTime > 1000) {
+		lastLoopTime = tickTime;
+		elapsedSecs++;
+	}
+	contextGame.font = "30px Verdana";
+	contextGame.fillText(elapsedSecs.toString(), 30, 30);
+}
 function paintImageRow(sprite, start, end, y) {
 	for (var x = start; x < end; x += 24) {
 		paintImage(sprite, x, y);
