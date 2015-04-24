6b24453a6f09dbabf49aaa4091b2c85d805fe3b6
Andris Klaipiņš <teloah@gmail.com>
2015-04-24 22:58:43 +0200

    starting to add handling of keys; adding game object

--- scripts/game.js
+++ scripts/game.js
@@ -1,5 +1,38 @@
 "use strict"
 
+var game = {
+	lastLoopTime : new Date(),
+	elapsedSecs : 0,
+	frames : 0,
+	lastFrames : 0
+}
+
+var keys = {
+	right : {
+		down: false
+	},
+	left : {
+		down: false
+	}
+}
+
+window.addEventListener("keydown", handleKeyDown);
+window.addEventListener("keyup", handleKeyUp);
+
+function handleKeyDown(event) {
+	if (event.which === 39) {
+		keys.right.down = true;
+		contextGame.globalAlpha = 0.5;
+	}
+}
+
+function handleKeyUp(event) {
+	if (event.which === 39) {
+		keys.right.down = false;
+		contextGame.globalAlpha = 1;
+	}
+}
+
 var canvasGame = document.getElementById("maingame"),
 	contextGame = canvasGame.getContext("2d"),
 	gfx_bricks = { left: 2, width: 24 },
@@ -10,17 +43,12 @@ var canvasGame = document.getElementById("maingame"),
 	gfx_zombie = { left: 123, width: 16 },
 	gfx_player = { left: 142, width: 15 },
 	gfx_box = { left: 160, width: 15 },
-	lastLoopTime = new Date(),
-	elapsedSecs = 0,
 	requestAnimFrame = window.requestAnimationFrame,
-	frames = 0,
-	lastFrames = 0,
 	imgSprites = new Image();
 imgSprites.src = "images/sprites.png";
 imgSprites.onload = init;
 
 function init() {
-	// contextGame.globalAlpha = 0.5;
 	requestAnimFrame(gameLoop);
 	console.log("initialized");
 }
@@ -37,6 +65,7 @@ function paintBorders() {
 }
 
 function gameLoop() {
+	document.getElementById("score").innerHTML = "<p>Score: " + game.elapsedSecs.toString() + "</p>";
 	contextGame.clearRect(0, 0, canvasGame.width, canvasGame.height);
 	paintBorders();
 	paintElapsedTime();
@@ -45,18 +74,19 @@ function gameLoop() {
 
 function paintElapsedTime() {
 	var tickTime = new Date();
-	if (tickTime - lastLoopTime > 1000) {
-		lastFrames = frames;
-		frames = 0;
-		lastLoopTime = tickTime;
-		elapsedSecs++;
+	if (tickTime - game.lastLoopTime > 1000) {
+		game.lastFrames = game.frames;
+		game.frames = 0;
+		game.lastLoopTime = tickTime;
+		game.elapsedSecs++;
 	}
-	frames++;
+	game.frames++;
 	contextGame.font = "30px Verdana";
-	contextGame.fillText(elapsedSecs.toString(), 24, canvasGame.height - 24);
+	contextGame.fillText(game.elapsedSecs.toString(), 24, canvasGame.height - 24);
 	contextGame.font = "15px Verdana";
-	contextGame.fillText(lastFrames.toString(), 24, canvasGame.height - 50);
+	contextGame.fillText(game.lastFrames.toString(), 24, canvasGame.height - 50);
 }
+
 function paintImageRow(sprite, start, end, y) {
 	for (var x = start; x < end; x += 24) {
 		paintImage(sprite, x, y);
